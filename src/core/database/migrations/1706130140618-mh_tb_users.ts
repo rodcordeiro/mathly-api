f38b2c23be5d4f6bb5913fcfa0d806992839fdd0
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-11-14 12:45:44 -0300

    Update and rename 001-mh_tb_users.ts to 1706130140618-mh_tb_users.ts

--- src/core/database/migrations/1706130140618-mh_tb_users.ts
+++ src/core/database/migrations/1706130140618-mh_tb_users.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class MhTbUsers001 implements MigrationInterface {
+export class MhTbUsers1706130140618 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
